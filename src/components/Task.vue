32d52bc1f8b676fe266af6ff0c985ec81f7070d0
Mateusz Łubianka <lubiankamateusz@gmail.com>
2020-05-06 12:04:17 +0200

    Add transition-group to task list

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="list-group-item task fadeInLeft animated blue lighten-5">
+    <div class="list-group-item task blue lighten-5">
         <p class="mb-0">
             <i class="fab fa-slack-hash fa-2x mr-4 mr-4 blue-gradient p-3 white-text rounded" aria-hidden="true"></i>
             <slot></slot>
--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="container list-group-flush task-list">
-        <Task v-for="(task, index) in tasks" @click.native="removeTask(index)" :key="index">{{task}}</Task>
+        <transition-group name="fade">
+            <Task v-for="(task, index) in tasks" @click.native="removeTask(index)" :key="index">{{task}}</Task>
+        </transition-group>
     </div>
 </template>
 
@@ -23,5 +25,34 @@ export default {
 <style scoped>
     .task-list {
         max-width: 800px;
+    }
+    .fade-enter {
+        opacity: 0;
+    }
+    .fade-enter-active {
+        animation: slide-in 0.7s ease-in-out forwards;
+        transition: opacity 0.7s;
+    }
+    .fade-leave-active {
+        animation: slide-out 0.7s ease-in-out forwards;
+        transition: opacity 0.7s;
+        opacity: 0;
+    }
+    
+    @keyframes slide-in {
+        from{
+            transform: translateX(-80px);
+        }
+        to {
+            transform: translateX(0);
+        }
+    }
+    @keyframes slide-out {
+        from{
+            transform: translateX(0);
+        }
+        to {
+            transform: translateX(-80px);
+        }
     }
 </style>
